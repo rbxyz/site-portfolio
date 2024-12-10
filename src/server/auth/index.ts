b84bc42294f62000c1ebb977318e1f2602387f70
rbxyz <rbcr4z1@gmail.com>
2024-12-10 08:32:35 -0300

    remove cache

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth";
-import { cache } from "react";
+import { unstable_cache } from "next/cache"; // Usar o unstable_cache
 
 import { authConfig } from "./config";
 
 const { auth: uncachedAuth, handlers, signIn, signOut } = NextAuth(authConfig);
 
-const auth = cache(uncachedAuth);
+const auth = unstable_cache(uncachedAuth, ['auth']); // Chave para cache
 
 export { auth, handlers, signIn, signOut };
