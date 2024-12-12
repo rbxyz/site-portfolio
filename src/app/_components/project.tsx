f2874d565429ec3f10f4c381109916883882618e
rbxyz <rbcr4z1@gmail.com>
2024-12-11 21:15:42 -0300

    changed color

--- src/app/_components/project.tsx
+++ src/app/_components/project.tsx
@@ -40,7 +40,7 @@ const projects: Project[] = [
 
 export function ProjetosSection() {
   return (
-    <section className="py-20 bg-gray-800 text-gray-100">
+    <section className="py-20  text-gray-100">
       <div className="container mx-auto px-4 md:px-6">
         <h2 className="text-3xl font-bold text-center mb-8">Projetos em Destaque</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
